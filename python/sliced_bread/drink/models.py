203f7e549bea126de371150ceaeb3b9b92742ae8
KeiferE <keifer.edelmayer@gmail.com>
2021-05-14 23:05:42 -0700

    Add templates and view for drink order
    Two templates, one for ordering information. The other for confirmation
    of details.
    
    [Ticket: X]

--- python/sliced_bread/drink/models.py
+++ python/sliced_bread/drink/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+import string
+import random
+
 # Create your models here.
+
+def unique_code_generator():
+        length = 6
+        while True:
+            code = ''.join(random.choices(string.ascii_uppercase, k=length))
+            if OrderModel.objects.filter(code=code).count() == 0:
+                break
+        return code
+
+def name_generator():
+    name = ''.join(random.choices(string.ascii_letters, k=4))
+    return name
+class OrderModel(models.Model):
+    code = models.CharField(max_length=8, default='', unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=15, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    slug = models.SlugField(null=True)
+
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
+
+    def get_absolute_url(self):
+        return reverse('order_detail', kwargs={'slug': self.slug}) 
--- python/sliced_bread/sliced_bread/urls.py
+++ python/sliced_bread/sliced_bread/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from drink.views import Index
+from drink.views import Index, Order
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
+    path('order/', Order.as_view(), name='order')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
