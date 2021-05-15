867567299f305d713274fd946c11dd085fe1bedb
KeiferE <keifer.edelmayer@gmail.com>
2021-05-14 22:05:02 -0700

    Add basic index placeholder
    [Ticket: X]

--- python/sliced_bread/sliced_bread/settings.py
+++ python/sliced_bread/sliced_bread/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost',]
 # Application definition
 
 INSTALLED_APPS = [
+    'drink',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
