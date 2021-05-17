6e5c5c4fd27488543b4ef2ba8cfa3b85634acd39
vidushi mohan <vidzimohan@yahoo.co.in>
2021-05-17 15:35:24 -0400

    second commit excluding history folder

--- flask_project/.history/form_20210515213129.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class checkoutForm(FlaskForm):
-    first_name = StringField("", validators = [DataRequired()],default = "Jane")
-    last_name = StringField("",validators=[DataRequired()],default="Joe")
-    submit = SubmitField("Checkout")
--- flask_project/.history/form_20210515221017.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class checkoutForm(FlaskForm):
-    name = StringField("First Name", validators = [DataRequired()])
-    last_name = StringField("Second Name",validators=[DataRequired()])
-    submit = SubmitField("Checkout")
