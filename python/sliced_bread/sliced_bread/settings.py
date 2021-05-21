68088b714f32ea70d7a2c119dfbaf5d4949854db
Katelyn Van Gorp <katelyn.vangorp@gmail.com>
2021-05-21 16:57:11 -0700

    Replace database with placeholder - It looks like there is some issue with the app finding the db file -  Setting up a placeholder database file for now

--- python/sliced_bread/sliced_bread/settings.py
+++ python/sliced_bread/sliced_bread/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*c^pug8gq7zp@d$ed$g)q*t0fy%1_h9rizq^fi*2%5ot=w4+$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'sliced_bread.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database.sqlite3',
     }
 }
 
