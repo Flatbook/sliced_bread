5bcfc68f012b2550dfb970dda0fdb393774478c7
Katelyn Van Gorp <katelyn.vangorp@gmail.com>
2021-05-21 18:25:06 -0700

    Add Order form and tests

--- python/sliced_bread/beverage/tests.py
+++ python/sliced_bread/beverage/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Order
+from .forms import OrderForm
 
 class OrderModelTests(TestCase):
     def test_default_customer_name(self):
@@ -9,3 +10,21 @@ class OrderModelTests(TestCase):
     def test_default_quantity(self):
         order = Order({'city': 'Halifax', 'province': 'Nova Scotia', 'country': 'Canada'})
         self.assertEqual(order.quantity, 1)
+
+
+class OrderFormTests(TestCase):
+    def test_form_invalid_when_missing_city(self):
+        form = OrderForm({'province': 'Alberta', 'country': 'Canada'})
+        self.assertFalse(form.is_valid())
+
+    def test_form_invalid_when_missing_province(self):
+        form = OrderForm({'city': 'Calgary', 'country': 'Canada'})
+        self.assertFalse(form.is_valid())
+
+    def test_form_invalid_when_missing_country(self):
+        form = OrderForm({'city': 'Calgary', 'province': 'Alberta'})
+        self.assertFalse(form.is_valid())
+
+    def test_form_invalid_when_negative_quantity(self):
+        form = OrderForm({'quantity': -2, 'city': 'Calgary', 'province': 'Alberta', 'country': 'Canada'})
+        self.assertFalse(form.is_valid())
