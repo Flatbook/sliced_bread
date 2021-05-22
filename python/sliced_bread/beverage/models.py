94c08d7beca9e6c52a9588090009415dd7d0a2d4
Katelyn Van Gorp <katelyn.vangorp@gmail.com>
2021-05-21 18:09:54 -0700

    Add Order model and tests

--- python/sliced_bread/beverage/models.py
+++ python/sliced_bread/beverage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Order(models.Model):
+    customer = models.CharField(max_length=50, default='Customer')
+    quantity = models.IntegerField(default=1)
+    city = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    confirmation_number = models.UUIDField(unique=True, default=uuid.uuid4)
+
+    def __str__(self):
+        return f'Customer: {self.customer}, Quantity: {self.quantity}, City: {self.city}, Province: {self.province}, Country: {self.country}'
--- python/sliced_bread/beverage/tests.py
+++ python/sliced_bread/beverage/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Order
 
-# Create your tests here.
+class OrderModelTests(TestCase):
+    def test_default_customer_name(self):
+        order = Order({'city': 'Halifax', 'province': 'Nova Scotia', 'country': 'Canada'})
+        self.assertEqual(order.customer, 'Customer')
+
+    def test_default_quantity(self):
+        order = Order({'city': 'Halifax', 'province': 'Nova Scotia', 'country': 'Canada'})
+        self.assertEqual(order.quantity, 1)
