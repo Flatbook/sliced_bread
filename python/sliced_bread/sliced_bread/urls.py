ad57db30c8e6154f5bfeb3941ba8cda86e408a25
Katelyn Van Gorp <katelyn.vangorp@gmail.com>
2021-05-21 20:50:43 -0700

    Change app root url

--- python/sliced_bread/sliced_bread/urls.py
+++ python/sliced_bread/sliced_bread/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('beverage/', include('beverage.urls')),
+    path('', include('beverage.urls')),
     path('admin/', admin.site.urls),
 ]
