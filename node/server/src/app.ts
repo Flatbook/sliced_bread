098d7d3bb4ed9160dd49413cd98f1f6b93366f44
Galit Gerasimov <galit.gerasimov3@gmail.com>
2022-02-06 15:00:04 -0500

    added express backend server

--- node/server/src/app.ts
+++ node/server/src/app.ts
@@ -1,12 +1,23 @@
 import express, { Application, Request, Response } from "express";
 
+const cors = require("cors");
+
 const app: Application = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("HELLO");
 });
 
 app.post("/order/:id", (req: Request, res: Response) => {
-  res.send("HELLO");
+  const id = req.params.id;
+  console.log("REQ", req.body);
+  res.send(id);
 });
 app.listen(5002, () => console.log("Server running"));
